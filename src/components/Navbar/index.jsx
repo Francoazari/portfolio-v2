5c61e4ec17fbee6255aff4342e0d15638bc18a8f
ismaFerrari <ismaelferrari97@gmail.com>
2022-11-29 14:54:15 -0300

    Changing text in NAV

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,7 +7,7 @@ function Navbar() {
             <ul>
                 <li>Inicio</li>
                 <li>About me</li>
-                <li></li>
+                <li>Projects</li>
                 <li>Contacto</li>
             </ul>
         </nav>
