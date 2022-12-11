550c64bb22bdcd8252b5c8831358167d7f7590c6
Franco <franco@miracledevs.com>
2022-12-11 11:55:16 -0300

    Minor fixes

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -71,94 +71,97 @@ function Portfolio() {
                     </section>
                 </a>
 
-                <a href="/#" name="about-me">
-                    <section className={styles.aboutMe}>
-                        <h2>About me</h2>
-                        <div className={styles.information}>
-                            <div className={styles.image}>{/* <img src={} alt={} /> */}</div>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi enim laboriosam optio velit earum! Rem ipsa repudiandae corrupti,
-                                debitis iste sequi quam similique necessitatibus ullam distinctio repellat? Laboriosam, nemo ipsum. Lorem ipsum, dolor sit amet
-                                consectetur adipisicing elit. Dignissimos esse nihil ratione quis odit doloremque aspernatur! Provident asperiores, qui
-                                inventore, rerum assumenda cumque reprehenderit voluptate alias, distinctio corporis nam numquam!
-                            </p>
-                        </div>
-                    </section>
-                </a>
+                <section className={styles.aboutMe}>
+                    <a href="/#" name="about-me">
+                        {" "}
+                    </a>
+                    <h2>ABOUT ME</h2>
+                    <div className={styles.information}>
+                        <div className={styles.image}>{/* <img src={} alt={} /> */}</div>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi enim laboriosam optio velit earum! Rem ipsa repudiandae corrupti,
+                            debitis iste sequi quam similique necessitatibus ullam distinctio repellat? Laboriosam, nemo ipsum. Lorem ipsum, dolor sit amet
+                            consectetur adipisicing elit. Dignissimos esse nihil ratione quis odit doloremque aspernatur! Provident asperiores, qui inventore,
+                            rerum assumenda cumque reprehenderit voluptate alias, distinctio corporis nam numquam!
+                        </p>
+                    </div>
+                </section>
 
-                <a href="/#" name="projects">
-                    <section className={styles.experience}>
-                        <h2>Experience</h2>
-                        <ul className={styles.experienceList}>
-                            <li>
-                                <span class={styles.heading}>Experience 1</span>
-                                <span class={styles.period}>Enero 2022 - Diciembre 2022</span>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia aliquid minus sit iusto architecto recusandae non
-                                    consectetur doloribus, harum dignissimos, deserunt accusantium? Possimus, explicabo reiciendis illo minima non blanditiis!
-                                </p>
-                                <ul className={styles.experienceSkill}>
-                                    <li>HTML</li>
-                                    <li>CSS</li>
-                                    <li>Javascript</li>
-                                    <li>React</li>
-                                </ul>
-                            </li>
-                            <li>
-                                <span class={styles.heading}>Experience 2</span>
-                                <span class={styles.period}>Enero 2022 - Diciembre 2022</span>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia aliquid minus sit iusto architecto recusandae non
-                                    consectetur doloribus, harum dignissimos, deserunt accusantium? Possimus, explicabo reiciendis illo minima non blanditiis!
-                                </p>
-                                <ul className={styles.experienceSkill}>
-                                    <li>HTML</li>
-                                    <li>CSS</li>
-                                    <li>Javascript</li>
-                                    <li>React</li>
-                                </ul>
-                            </li>
-                            <li>
-                                <span class={styles.heading}>Experience 3</span>
-                                <span class={styles.period}>Enero 2022 - Diciembre 2022</span>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia aliquid minus sit iusto architecto recusandae non
-                                    consectetur doloribus, harum dignissimos, deserunt accusantium? Possimus, explicabo reiciendis illo minima non blanditiis!
-                                </p>
-                                <ul className={styles.experienceSkill}>
-                                    <li>HTML</li>
-                                    <li>CSS</li>
-                                    <li>Javascript</li>
-                                    <li>React</li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </section>
-                </a>
+                <section className={styles.experience}>
+                    <h2>EXPERIENCE</h2>
+                    <ul className={styles.experienceList}>
+                        <li>
+                            <span className={styles.heading}>Experience 1</span>
+                            <span className={styles.period}>Enero 2022 - Diciembre 2022</span>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia aliquid minus sit iusto architecto recusandae non
+                                consectetur doloribus, harum dignissimos, deserunt accusantium? Possimus, explicabo reiciendis illo minima non blanditiis!
+                            </p>
+                            <ul className={styles.experienceSkill}>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>Javascript</li>
+                                <li>React</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <span className={styles.heading}>Experience 2</span>
+                            <span className={styles.period}>Enero 2022 - Diciembre 2022</span>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia aliquid minus sit iusto architecto recusandae non
+                                consectetur doloribus, harum dignissimos, deserunt accusantium? Possimus, explicabo reiciendis illo minima non blanditiis!
+                            </p>
+                            <ul className={styles.experienceSkill}>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>Javascript</li>
+                                <li>React</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <span className={styles.heading}>Experience 3</span>
+                            <span className={styles.period}>Enero 2022 - Diciembre 2022</span>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia aliquid minus sit iusto architecto recusandae non
+                                consectetur doloribus, harum dignissimos, deserunt accusantium? Possimus, explicabo reiciendis illo minima non blanditiis!
+                            </p>
+                            <ul className={styles.experienceSkill}>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>Javascript</li>
+                                <li>React</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </section>
 
                 {projects && projects.length > 0 && (
                     <section className={styles.works} id="projects">
-                        <h2>Projects</h2>
+                        <a href="/#" name="projects">
+                            {" "}
+                        </a>
+                        <h2>PROJECTS</h2>
                         {projects.map((project, index) => {
                             return <ProjectCard key={index} projectInformation={project} />;
                         })}
                     </section>
                 )}
 
-                <a href="/#" name="get-in-touch">
-                    <section className={styles.contact}>
-                        <h2>GET IN TOUCH</h2>
-                        <form>
-                            <input name="name" type="text" placeholder="Name"></input>
-                            <input name="email" type="email" placeholder="Email"></input>
-                            <input name="phone" placeholder="Phone number"></input>
-                            <textarea name="message" placeholder="Message"></textarea>
-                            <button type="submit" value="">
-                                Send message
-                            </button>
-                        </form>
-                    </section>
-                </a>
+                <section className={styles.contact}>
+                    <a href="/#" name="get-in-touch">
+                        {" "}
+                    </a>
+                    <h2>GET IN TOUCH</h2>
+                    <form>
+                        <input name="name" type="text" placeholder="Name"></input>
+                        <input name="email" type="email" placeholder="Email"></input>
+                        <input name="phone" placeholder="Phone number"></input>
+                        <textarea name="message" placeholder="Message"></textarea>
+                        <button type="submit" value="">
+                            Send message
+                        </button>
+                    </form>
+                </section>
             </main>
 
             <Footer />
