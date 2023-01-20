39a8d30b2f5de17f52f05d6d694ab8978fde9dc5
Franco <franco@miracledevs.com>
2023-01-20 11:14:51 -0300

    Minor fix on portfolio headings and card projects

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -4,39 +4,37 @@ function ProjectCard({ projectInformation }) {
     return (
         projectInformation && (
             <div className={styles.card}>
-                <div className={styles.cardContainer}>
-                    <div className={styles.cardImage}>
-                        <img src={"./assets/works/example.jpg"} alt={"asd"} />
-                    </div>
-                    <div className={styles.cardInfo}>
-                        <div className={styles.information}>
-                            <h3>{projectInformation.title}</h3>
-                            <div className={styles.paragraph}>
-                                <p>{projectInformation.paragraph}</p>
-                            </div>
-                            {projectInformation.skillTags && (
-                                <div className={styles.skillTags}>
-                                    <ul>
-                                        {projectInformation.skillTags.map((skill, index) => {
-                                            return <li key={index}>{skill.toUpperCase()}</li>;
-                                        })}
-                                    </ul>
-                                </div>
-                            )}
+                <div className={styles.cardImage}>
+                    <img src={"./assets/works/example.jpg"} alt={"asd"} />
+                </div>
+                <div className={styles.cardInfo}>
+                    <div className={styles.information}>
+                        <h3>{projectInformation.title}</h3>
+                        <div className={styles.paragraph}>
+                            <p>{projectInformation.paragraph}</p>
                         </div>
-                        {projectInformation.worksLinks && (
-                            <div className={styles.worksLinks}>
-                                {projectInformation.worksLinks.map((workLink, index) => {
-                                    return (
-                                        <a key={index} href={workLink.url}>
-                                            <img src={workLink.image} alt={workLink.alternativeText} />
-                                            <p>{workLink.label}</p>
-                                        </a>
-                                    );
-                                })}
+                        {projectInformation.skillTags && (
+                            <div className={styles.skillTags}>
+                                <ul>
+                                    {projectInformation.skillTags.map((skill, index) => {
+                                        return <li key={index}>{skill.toUpperCase()}</li>;
+                                    })}
+                                </ul>
                             </div>
                         )}
                     </div>
+                    {projectInformation.worksLinks && (
+                        <div className={styles.worksLinks}>
+                            {projectInformation.worksLinks.map((workLink, index) => {
+                                return (
+                                    <a key={index} href={workLink.url}>
+                                        <img src={workLink.image} alt={workLink.alternativeText} />
+                                        <p>{workLink.label}</p>
+                                    </a>
+                                );
+                            })}
+                        </div>
+                    )}
                 </div>
             </div>
         )
