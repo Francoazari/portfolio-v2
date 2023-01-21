36cc504845645c8642775b739b7951dd00c33e14
Franco <franco@miracledevs.com>
2023-01-21 02:08:33 -0300

    compressing background images and adding animated elements

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -6,6 +6,7 @@ import Footer from "../Footer";
 import Navbar from "../Navbar";
 import ProjectCard from "../ProjectCard";
 import styles from "./Portfolio.module.scss";
+import animatedStyle from "../../styles/animated-element.module.scss";
 
 function Portfolio() {
     let nombre = "Franco Azari";
@@ -120,9 +121,9 @@ function Portfolio() {
                     <img src={isDesktop ? "./assets/background-desktop.jpg" : "./assets/background-mobile.jpg"} alt="Background section" />
 
                     <div className={styles.headingContainer}>
-                        <span>Hello, I'm</span>
-                        <h1>{nombre.toUpperCase()}</h1>
-                        <span>Nice to meet you!</span>
+                        <span className={clsx(animatedStyle.animatedElement, animatedStyle.order1)}>Hello, I'm</span>
+                        <h1 className={clsx(animatedStyle.animatedElement, animatedStyle.order2)}>{nombre.toUpperCase()}</h1>
+                        <span className={clsx(animatedStyle.animatedElement, animatedStyle.order3)}>Nice to meet you!</span>
                     </div>
 
                     <svg className={styles.arrow} data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
