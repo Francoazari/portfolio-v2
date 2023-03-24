90c457b50d1398cfa268ab94bdee13ffd4526602
Franco <franco@miracledevs.com>
2023-03-24 18:32:32 -0300

    Taglist fix

--- src/components/TagList/index.jsx
+++ src/components/TagList/index.jsx
@@ -4,7 +4,10 @@ import styles from "./TagList.module.scss";
 function TagList({ list, backgroundColor, fontColor, align }) {
     console.log();
     return (
-        <ul style={{ "--background-color": `${backgroundColor}`, "--font-color": `${fontColor}` }} className={clsx({ [styles.alignRight]: align === "right" })}>
+        <ul
+            style={{ "--background-color": `${backgroundColor}`, "--font-color": `${fontColor}` }}
+            className={clsx(styles.taglist, { [styles.alignRight]: align === "right" })}
+        >
             {list.map((item, index) => {
                 return <li key={index}>{item}</li>;
             })}
